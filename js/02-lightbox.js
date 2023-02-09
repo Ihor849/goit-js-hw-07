dcac31c772f5d11dfece879ea7355236fd233dd7
Ihor849 <333marchelo333@gmail.com>
2023-02-09 18:16:19 +0200

    Закінчює 2ге завдання 09.02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+// / +++++++++++++++++++++  В А Р И А Н Т - 1 через С Т Р О К У ================
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+const imageMarkup = createImageCardsMarkup(galleryItems);
+
+function createImageCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  
+    <a class="gallery__link" href = "${original}" >
+      <img
+        class= "gallery__image"
+        src= ${preview}
+        alt= ${description}
+        title= "Beautiful Image"
+      />
+    </a>`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", imageMarkup);
+
+gallery.addEventListener("click", onbrowsesClickCollection);
+
+function onbrowsesClickCollection(evt) {
+  evt.preventDefault();
+}
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionPosition: "bottom",
+  captionDelay: "250",
+  captionsData: "alt",
+});
+console.dir(lightbox.options);
